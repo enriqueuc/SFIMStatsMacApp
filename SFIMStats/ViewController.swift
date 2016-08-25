1657f5e3f5884bc283584c857752dd19b7ae7d9a
Enrique Ugalde <enrique@ekt1136648mac.local>
2016-08-25 18:49:42 -0500

    - Set up labels and clear content - Set up button actions - Set up main action

--- SFIMStats/ViewController.swift
+++ SFIMStats/ViewController.swift
@@ -10,9 +10,16 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var lbl_inicio: NSTextField!
+    @IBOutlet weak var lbl_paquetes: NSTextField!
+    @IBOutlet weak var lbl_servicios: NSTextField!
+    @IBOutlet weak var lbl_galeria: NSTextField!
+    @IBOutlet weak var lbl_mapa: NSTextField!
+    @IBOutlet weak var lbl_contacto: NSTextField!
+    @IBOutlet weak var lbl_correos: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +29,26 @@ class ViewController: NSViewController {
         }
     }
 
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        lbl_inicio.stringValue = ""
+        lbl_paquetes.stringValue = ""
+        lbl_servicios.stringValue = ""
+        lbl_galeria.stringValue = ""
+        lbl_mapa.stringValue = ""
+        lbl_contacto.stringValue = ""
+        lbl_correos.stringValue = ""
+        obtenerParams()
+    }
+    
+    func obtenerParams() {
+    }
+    
+    @IBAction func btn_actualizar(sender: AnyObject) {
+    }
+    
+    @IBAction func btn_copiar(sender: AnyObject) {
+    }
 
 }
 
