8ddbe9fe0d85509ee8d7e21bac28845f86f4a0df
Enrique Ugalde <enrique@ekt1136648mac.local>
2016-08-25 18:20:32 -0500

    Initial Commit

---
+++ SFIMStats/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SFIMStats
+//
+//  Created by Enrique Ugalde on 25/08/16.
+//  Copyright © 2016 Enrique Ugalde. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
